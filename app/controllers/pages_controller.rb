d932c40160315baaabc7202871efa35a4005a189
Patrick <dongyang0216@gmail.com>
2024-04-24 02:32:42 +0800

    adjust some style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    if user_signed_in? || admin_user_signed_in?
+    if user_signed_in?
       flash[:alert] = "You are already signed in."
       redirect_to news_path
     end
